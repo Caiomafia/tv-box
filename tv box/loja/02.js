2e36a606608e7171942c2dd3a006b2f18fde7e27
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2022-03-28 11:04:10 -0300

    add commit

---
+++ tv box/loja/02.js
@@ -0,0 +1,10 @@
+const nome = 'Caio Mafra';
+const sobrenome = 'alexandria' ;
+const idade = '18';
+const alturaEmM ='1,8';
+const peso = '90kg';
+let imc =  peso/ (alturaEmM * alturaEmM); 
+console.log(imc);/*peso/(altura + altura) */
+
+
+console.log(nome.sobrenome ,'ele tem', idade,nome ,'ele pesa', peso, 'kg ' , alturaEmM , 'com isso sua massa corporal e ' , imc, 'o ano que ele nasceu e ', anodenascimento);
---
+++ tv box/loja/README.md
@@ -0,0 +1,2 @@
+# Loja
+criação de uma loja usando JS.CSS.HTML
